4669b48c8f20c5a5dfa5d1e3636e16ce9fbca80c
20537145 <141738221+20537145@users.noreply.github.com>
2023-11-05 12:17:49 +0100

    api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [quotes,setQuotes]=useState('')
+  const [author, setAuthor]=useState('')
+  const getQuotes = () => {
+    axios
+      .get("https://api.quotable.io/random")
+      .then((ref) => {
+        setQuotes(ref.data.content)
+        setAuthor(ref.data.author)
+        console.log(ref.data.content);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="btn-1" onClick={getQuotes}><b>how about to shut the fuck up</b></button>
+      <h2>{quotes}</h2>
+      <h3 style={{color:'red'}}>{author}</h3>
     </div>
   );
 }
